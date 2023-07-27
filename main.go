bfc382295cf9d9824d9224845006f5cb27280e45
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-07-27 18:20:30 +0500

    load balancing working

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "<h1>Hello from Docker!</h1><p>Hostname: "+hostname+"</p>")
+	})
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
+
+// Simple implementation of an integer minimum
+// Adapted from: https://gobyexample.com/testing-and-benchmarking
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
